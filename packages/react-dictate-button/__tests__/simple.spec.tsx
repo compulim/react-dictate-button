16a4a6c596db39f8f0dd16a3d926fe73f7fc7aa3
William Wong <compulim@hotmail.com>
2025-01-28 19:08:07 -0800

    Update test to use mocked SpeechRecognition

--- packages/react-dictate-button/__tests__/simple.spec.tsx
+++ packages/react-dictate-button/__tests__/simple.spec.tsx
@@ -3,29 +3,63 @@
 import { act, fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { DictateButton } from 'react-dictate-button';
-import { SpeechGrammarList, SpeechRecognition } from 'react-dictate-button-mocked-speech-recognition';
+import {
+  SpeechRecognition,
+  SpeechRecognitionAlternative,
+  SpeechRecognitionEvent,
+  SpeechRecognitionResult,
+  SpeechRecognitionResultList
+} from 'react-dictate-button-mocked-speech-recognition';
 
-test('continuous scenario', async () => {
-  const constructSpeechRecognition = jest
-    .fn<SpeechRecognition, []>()
-    .mockImplementationOnce(() => new SpeechRecognition());
+test('simple scenario', async () => {
+  let start;
+  const constructSpeechRecognition = jest.fn().mockImplementationOnce(() => {
+    const speechRecognition = new SpeechRecognition();
+
+    start = jest.spyOn(speechRecognition, 'start');
+
+    return speechRecognition;
+  });
   const handleDictate = jest.fn();
 
   render(
     <DictateButton
       onDictate={handleDictate}
-      speechGrammarList={SpeechGrammarList}
+      speechGrammarList={window.SpeechGrammarList}
       speechRecognition={constructSpeechRecognition}
     >
       Click me
     </DictateButton>
   );
 
-  await act(() => fireEvent.click(screen.getByText('Click me')));
+  expect(constructSpeechRecognition).toHaveBeenCalledTimes(0);
+
+  act(() => fireEvent.click(screen.getByText('Click me')));
+
+  expect(constructSpeechRecognition).toHaveBeenCalledTimes(1);
+  expect(start).toHaveBeenCalledTimes(1);
+
+  act(() => {
+    const /** @type {SpeechRecognition} */ speechRecognition = constructSpeechRecognition.mock.results[0]?.value;
+
+    speechRecognition.dispatchEvent(new Event('start', {}));
+    speechRecognition.dispatchEvent(new Event('audiostart', {}));
+    speechRecognition.dispatchEvent(new Event('soundstart', {}));
+    speechRecognition.dispatchEvent(new Event('speechstart', {}));
+
+    speechRecognition.dispatchEvent(
+      new SpeechRecognitionEvent('result', {
+        results: new SpeechRecognitionResultList([
+          new SpeechRecognitionResult([new SpeechRecognitionAlternative(0.9, 'Hello, World!')], true)
+        ])
+      })
+    );
+
+    speechRecognition.dispatchEvent(new Event('speechend', {}));
+    speechRecognition.dispatchEvent(new Event('soundend', {}));
+    speechRecognition.dispatchEvent(new Event('audioend', {}));
+    speechRecognition.dispatchEvent(new Event('end', {}));
+  });
 
   expect(handleDictate).toHaveBeenCalledTimes(1);
-
-  console.log(handleDictate.mock.calls[0]);
-
-  const speechRecognition = constructSpeechRecognition.mock.results[0].value as SpeechRecognition;
 });

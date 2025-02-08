a3b24ffb90dfe064ab60adef4a864d4b7d4b978e
William Wong <compulim@hotmail.com>
2025-02-07 16:59:30 -0800

    Fix ESLint

--- packages/mocked-speech-recognition/src/SpeechRecognitionResult.ts
+++ packages/mocked-speech-recognition/src/SpeechRecognitionResult.ts
@@ -3,10 +3,12 @@
 
 import SpeechRecognitionAlternative from './SpeechRecognitionAlternative.ts';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export default interface SpeechRecognitionResult {
   fromFinalized(...items: SpeechRecognitionAlternative[]): SpeechRecognitionResult;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export default class SpeechRecognitionResult
   extends Array<SpeechRecognitionAlternative>
   implements SpeechRecognitionResult

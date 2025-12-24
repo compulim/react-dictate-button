2eb45c1c8afdee5dfc5fdd62cf0000ea1d390bec
@compulim <@compulim>
2025-12-24 01:43:03 +0000

    Bump scaffold

--- packages/react-dictate-button/tsup.config.ts
+++ packages/react-dictate-button/tsup.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    define: { IS_DEVELOPMENT: 'false' },
     dts: true,
     entry: {
-      'react-dictate-button': './src/index.ts',
-      'react-dictate-button.internal': './src/internal.ts'
+      'react-dictate-button': './src/index.ts'
     },
     format: ['cjs', 'esm'],
-    sourcemap: true
+    sourcemap: true,
+    target: 'esnext'
   }
 ]);
